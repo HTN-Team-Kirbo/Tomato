516e452e002bb73d2316c1e0b0e74efa44f3ae91
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-16 14:35:39 -0500

    reworked some code in main.dart to be less stupid

--- htn_tomato/lib/main.dart
+++ htn_tomato/lib/main.dart
@@ -12,84 +12,34 @@ void main() {
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return MaterialApp(
-              home: ErrorPage(title: "uwu, o no!"),
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("Error!");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return MyHomePage(title: "titeltext");
+            }
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MaterialApp(
-              home: MyHomePage(title: "titeltext"),
-            );
-          }
-
-          return MaterialApp(home: Loading(title: "FLOADING :PP"));
-        });
-  }
-}
-
-class ErrorPage extends StatefulWidget {
-  ErrorPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _ErrorPageState createState() => _ErrorPageState();
-}
-
-class _ErrorPageState extends State<ErrorPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Error',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Loading extends StatefulWidget {
-  Loading({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _LoadingState createState() => _LoadingState();
-}
-
-class _LoadingState extends State<Loading> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Loading',
-            ),
-          ],
-        ),
-      ),
+          }),
     );
   }
 }
