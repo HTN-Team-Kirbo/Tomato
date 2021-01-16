89781002797674f7f6ffec3e1b88465f68a6943e
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-16 16:39:38 -0500

    import changes

--- htn_tomato/lib/main.dart
+++ htn_tomato/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'flutterfire.dart';
 import 'unogs.dart';
 
