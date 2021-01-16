22bf86edb5b1cb49cc0bf32b4cddd35ce739e9e6
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-16 05:12:13 -0500

    Firebase core

--- htn_tomato/lib/main.dart
+++ htn_tomato/lib/main.dart
@@ -1,32 +1,94 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized()
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorPage();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyHomePage();
+        }
+
+        return Loading();
+      }
+    );
+  }
+}
+
+class ErrorPage extends StatefulWidget {
+  ErrorPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ErrorPageState createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Error',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Loading extends StatefulWidget {
+  Loading({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Loading',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
