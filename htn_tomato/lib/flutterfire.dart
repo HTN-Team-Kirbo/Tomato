0f2f227ef5d020e114805c5594f0b8cfe8c42cfb
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-16 19:54:51 -0500

    flutterfire stuff

--- htn_tomato/lib/flutterfire.dart
+++ htn_tomato/lib/flutterfire.dart
@@ -2,31 +2,59 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> checkAuth() async {
-  await Firebase.initializeApp();
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth;
 
-  FirebaseAuth auth = FirebaseAuth.instance;
-  if (auth.currentUser != null) {
-    print("Already signed in");
-  } else {
-    print("Signed Out");
-  }
-}
+  AuthenticationService(this._firebaseAuth);
 
-Future<void> signUp(email, password) async {
-  await Firebase.initializeApp();
+  Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  try {
-    UserCredential userCredential = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: password);
-    print("success!");
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'weak-password') {
-      print('The password provided is too weak.');
-    } else if (e.code == 'email-already-in-use') {
-      print('The account already exists for that email.');
+  Future<void> checkAuth() async {
+    await Firebase.initializeApp();
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    if (auth.currentUser != null) {
+      print("Already signed in");
+    } else {
+      print("Signed Out");
     }
-  } catch (e) {
-    print(e);
+  }
+
+  Future<String> signUp(String email, String password) async {
+    await Firebase.initializeApp();
+
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return "success!";
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        return 'The account already exists for that email.';
+      }
+    } catch (e) {
+      return e.message;
+    }
+    return 'An unknown error has occurred.';
+  }
+
+  Future<String> login(String email, String password) async {
+    await Firebase.initializeApp();
+
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return "success!";
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        return 'The account already exists for that email.';
+      }
+    } catch (e) {
+      return e.message;
+    }
+    return 'An unknown error has occurred.';
   }
 }
