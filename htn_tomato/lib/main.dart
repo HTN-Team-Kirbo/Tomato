07769c3a869755f25e50fd56dcc1199c3dda40fa
Biosaur <biosaur99@gmail.com>
2021-01-17 02:48:16 -0500

    Update main.dart

--- htn_tomato/lib/main.dart
+++ htn_tomato/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF141432),
         primarySwatch: Colors.blue,
         colorScheme: ColorScheme(
             primary: Colors.blue,
