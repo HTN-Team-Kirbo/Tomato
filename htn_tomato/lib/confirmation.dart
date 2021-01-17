8437887b09fdc14879480183b4061bec4bb73674
Biosaur <biosaur99@gmail.com>
2021-01-16 22:12:02 -0500

    Create confirmation.dart

---
+++ htn_tomato/lib/confirmation.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'pages.dart';
+
+class ConfirmationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData localTheme = Theme.of(context);
+
+    return Scaffold(
+        body: Center(
+            child: Column(
+                children: <Widget>[
+                  Text(
+                    "Check your inbox for a confirmation email!",
+                    style: localTheme.textTheme.headline1,
+                  ),
+                  Text(
+                    "Let's start setting your goals.",
+                    style: localTheme.textTheme.bodyText2,
+                  )
+                ]
+            )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => LoginScreen()),
+            );
+          },
+          child: Icon(Icons.arrow_forward),
+        )
+    );
+
+  }
+}
