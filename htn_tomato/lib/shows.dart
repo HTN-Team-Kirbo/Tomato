64b23c431ec121e0c31a8611b672a7ccdcc63769
Biosaur <biosaur99@gmail.com>
2021-01-16 19:58:10 -0500

    Separated Netflix show settings

---
+++ htn_tomato/lib/shows.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'pages.dart';
+
+APIService apiService = APIService();
+
+class ShowSetupPage extends StatefulWidget {
+  ShowSetupPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _ShowSetupState createState() => _ShowSetupState();
+}
+
+class _ShowSetupState extends State<ShowSetupPage> {
+  TextEditingController _controller;
+
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  List<dynamic> shows = [];
+
+  void _searchShowTitle(String title) async {
+    var foundShows = (await apiService.get(
+        endpoint: '/search', query: {"limit": "8", "query": title}))["results"];
+    setState(() {
+      shows = foundShows;
+    });
+  }
+
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Text(shows[index]["title"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Search for a Netflix show to watch"',
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: TextField(
+                  controller: _controller,
+                  onSubmitted: (String title) async {
+                    _searchShowTitle(title);
+                  }),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: shows.length,
+                itemExtent: 60.0,
+                itemBuilder: _listItemBuilder,
+                shrinkWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
