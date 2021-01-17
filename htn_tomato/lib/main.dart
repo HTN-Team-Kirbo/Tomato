cbb36283f70aaec5184af8fe55970c61df82f5c4
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-16 19:58:35 -0500

    clean main a bit

--- htn_tomato/lib/main.dart
+++ htn_tomato/lib/main.dart
@@ -15,32 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+            future: _initialization,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("Error!");
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                return MyHomePage(title: "titeltext uwu");
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }),
       ),
-      home: FutureBuilder(
-          future: _initialization,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text("Error!");
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              return MyHomePage(title: "titeltext uwu");
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
     );
   }
 }
