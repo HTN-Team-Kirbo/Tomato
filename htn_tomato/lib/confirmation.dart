5bca4a0d7b5bd708e11ded2aa110b511d06f8ea1
Jerry Guo <Spacepuffin7@gmail.com>
2021-01-17 07:57:21 -0500

    fixed routing again :P

--- htn_tomato/lib/confirmation.dart
+++ htn_tomato/lib/confirmation.dart
@@ -51,7 +51,7 @@ class ConfirmationPage extends StatelessWidget {
             if (verified) {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => CycleSetupPage()),
+                MaterialPageRoute(builder: (context) => ShowSetupPage()),
               );
             }
           },
